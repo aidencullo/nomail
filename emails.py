a6e7a1fc14d851ed85bbd1c80aa66393a009185e
Aiden Cullo <aidencullo@gmail.com>
2023-11-08 17:31:41 -0500

    sample panda export

--- emails.py
+++ emails.py
@@ -2,7 +2,6 @@ import imaplib
 import email
 from email.header import decode_header
 import os
-import sys
 from dotenv import load_dotenv
 
 import filtering
@@ -28,20 +27,17 @@ status, messages = imap.search(None, f'FROM {sender}')
 # to get all mails
 # status, messages = imap.search(None, "ALL")
 
-# convert messages to a list of email IDs
 messages = messages[0].split(b' ')
 
 for mail in messages:
-    _, msg = imap.fetch(mail, "(RFC822)")
-    # you can delete the for loop for performance if you have a long list of emails
-    # because it is only for printing the SUBJECT of target email to delete
+    status, msg = imap.fetch(mail, "(RFC822)")
+    if status != 'OK':
+        raise ValueError('unexpected http response')
     for response in msg:
         if isinstance(response, tuple):
             msg = email.message_from_bytes(response[1])
-            # decode the email subject
             subject = filtering.filter_header(decode_header(msg["Subject"]))
             if isinstance(subject, bytes):
-                # if it's a bytes type, decode to str
                 subject = subject.decode()
             print(f'deleting {subject}')
     # mark the mail as deleted
@@ -56,3 +52,55 @@ for mail in messages:
 # imap.close()
 # # logout from the account
 # imap.logout()
+
+data = {
+    'CHN': {'COUNTRY': 'China', 'POP': 1_398.72, 'AREA': 9_596.96,
+            'GDP': 12_234.78, 'CONT': 'Asia'},
+    'IND': {'COUNTRY': 'India', 'POP': 1_351.16, 'AREA': 3_287.26,
+            'GDP': 2_575.67, 'CONT': 'Asia', 'IND_DAY': '1947-08-15'},
+    'USA': {'COUNTRY': 'US', 'POP': 329.74, 'AREA': 9_833.52,
+            'GDP': 19_485.39, 'CONT': 'N.America',
+            'IND_DAY': '1776-07-04'},
+    'IDN': {'COUNTRY': 'Indonesia', 'POP': 268.07, 'AREA': 1_910.93,
+            'GDP': 1_015.54, 'CONT': 'Asia', 'IND_DAY': '1945-08-17'},
+    'BRA': {'COUNTRY': 'Brazil', 'POP': 210.32, 'AREA': 8_515.77,
+            'GDP': 2_055.51, 'CONT': 'S.America', 'IND_DAY': '1822-09-07'},
+    'PAK': {'COUNTRY': 'Pakistan', 'POP': 205.71, 'AREA': 881.91,
+            'GDP': 302.14, 'CONT': 'Asia', 'IND_DAY': '1947-08-14'},
+    'NGA': {'COUNTRY': 'Nigeria', 'POP': 200.96, 'AREA': 923.77,
+            'GDP': 375.77, 'CONT': 'Africa', 'IND_DAY': '1960-10-01'},
+    'BGD': {'COUNTRY': 'Bangladesh', 'POP': 167.09, 'AREA': 147.57,
+            'GDP': 245.63, 'CONT': 'Asia', 'IND_DAY': '1971-03-26'},
+    'RUS': {'COUNTRY': 'Russia', 'POP': 146.79, 'AREA': 17_098.25,
+            'GDP': 1_530.75, 'IND_DAY': '1992-06-12'},
+    'MEX': {'COUNTRY': 'Mexico', 'POP': 126.58, 'AREA': 1_964.38,
+            'GDP': 1_158.23, 'CONT': 'N.America', 'IND_DAY': '1810-09-16'},
+    'JPN': {'COUNTRY': 'Japan', 'POP': 126.22, 'AREA': 377.97,
+            'GDP': 4_872.42, 'CONT': 'Asia'},
+    'DEU': {'COUNTRY': 'Germany', 'POP': 83.02, 'AREA': 357.11,
+            'GDP': 3_693.20, 'CONT': 'Europe'},
+    'FRA': {'COUNTRY': 'France', 'POP': 67.02, 'AREA': 640.68,
+            'GDP': 2_582.49, 'CONT': 'Europe', 'IND_DAY': '1789-07-14'},
+    'GBR': {'COUNTRY': 'UK', 'POP': 66.44, 'AREA': 242.50,
+            'GDP': 2_631.23, 'CONT': 'Europe'},
+    'ITA': {'COUNTRY': 'Italy', 'POP': 60.36, 'AREA': 301.34,
+            'GDP': 1_943.84, 'CONT': 'Europe'},
+    'ARG': {'COUNTRY': 'Argentina', 'POP': 44.94, 'AREA': 2_780.40,
+            'GDP': 637.49, 'CONT': 'S.America', 'IND_DAY': '1816-07-09'},
+    'DZA': {'COUNTRY': 'Algeria', 'POP': 43.38, 'AREA': 2_381.74,
+            'GDP': 167.56, 'CONT': 'Africa', 'IND_DAY': '1962-07-05'},
+    'CAN': {'COUNTRY': 'Canada', 'POP': 37.59, 'AREA': 9_984.67,
+            'GDP': 1_647.12, 'CONT': 'N.America', 'IND_DAY': '1867-07-01'},
+    'AUS': {'COUNTRY': 'Australia', 'POP': 25.47, 'AREA': 7_692.02,
+            'GDP': 1_408.68, 'CONT': 'Oceania'},
+    'KAZ': {'COUNTRY': 'Kazakhstan', 'POP': 18.53, 'AREA': 2_724.90,
+            'GDP': 159.41, 'CONT': 'Asia', 'IND_DAY': '1991-12-16'}
+}
+
+columns = ('COUNTRY', 'POP', 'AREA', 'GDP', 'CONT', 'IND_DAY')
+
+import pandas as pd
+
+df = pd.DataFrame(data=data).T
+
+df.to_csv('data.csv')
