7d95d87d466e4fbe29b907fc98f9a515fbc9190c
Aiden Cullo <aidencullo@gmail.com>
2023-11-13 20:29:24 -0500

    write out summary of filter

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ username = e.get_username()
 password = e.get_password()
 
 sender = "info@email.meetup.com"
-                    
-a_filter = email_filter.Filter(username, password)
-emails = a_filter.delete_emails(sender)
 
-output.print_file()
+emails = ["fake@gmail.com"]
+# a_filter = email_filter.Filter(username, password)
+# emails = a_filter.delete_emails(sender)
+
+output.print_file(emails)
--- output.py
+++ output.py
@@ -1,6 +1,7 @@
 import pandas as pd
+from datetime import datetime
 
-def print_file(data=None, file_name='data.html'):
+def print_file(data=None):
     if data == None:
         data = []
     data = {
@@ -8,4 +9,9 @@ def print_file(data=None, file_name='data.html'):
     }
 
     df = pd.DataFrame(data)
+
+    # file_name = datetime.today().strftime('%Y-%m-%d_%H:%M:%S')
+    file_name = datetime.today().isoformat()
+    file_name = 'summaries/' + file_name + '.html'
+    
     df.to_html(file_name)
