bf1afd4fcb127e3d0bec721d743a40e989da19d1
Aiden Cullo <aidencullo@gmail.com>
2023-12-21 14:14:43 -0500

    complete action unittest

--- src/action.py
+++ src/action.py
@@ -1,6 +1,37 @@
+"""
+This module provides abstract base classes (ABC) and abstract
+methods for defining interfaces to be implemented by concrete
+classes. The ABC class and abstractmethod decorator are imported from
+the 'abc' module.
+
+Classes:
+- ABC: Abstract Base Class, the base class for all other abstract base classes.
+- abstractmethod: A decorator indicating that a method must be
+  implemented by concrete subclasses.
+
+Example usage:
+    from abc import ABC, abstractmethod
+
+    class MyAbstractClass(ABC):
+        @abstractmethod
+        def my_abstract_method(self):
+            pass
+
+    class MyConcreteClass(MyAbstractClass):
+        def my_abstract_method(self):
+            # Implementation of the abstract method
+            pass
+
+Note:
+    It is recommended to inherit from ABC when creating abstract base
+    classes and use the @abstractmethod decorator to define abstract
+    methods that must be implemented by subclasses.
+
+"""
 from abc import ABC, abstractmethod
 
 from .adapter import EmailImapAdapter
+
 
 class Action(ABC):
     """Action take on an email"""
@@ -12,11 +43,13 @@ class Action(ABC):
     def act(self, email):
         pass
 
+
 class ActionDelete(Action):
     """Delete email"""
 
     def act(self, email):
         self._imap.delete_msg(email)
+
 
 class ActionCopy(Action):
     """Copy email"""
@@ -24,11 +57,13 @@ class ActionCopy(Action):
     def act(self, email):
         self._imap.copy_msg(email)
 
+
 class ActionPrint(Action):
     """Print email"""
 
     def act(self, email):
         print(email.sender)
+
 
 class ActionMove(Action):
     """Move email"""
--- tests/test_action.py
+++ tests/test_action.py
@@ -1,8 +1,77 @@
+from unittest.mock import MagicMock
+
 import pytest
 
-from src.action import Action
+from src.action import (ActionDelete, ActionCopy, ActionPrint,
+                        ActionMove, Action)
+from src.adapter import EmailImapAdapter
 
-def test_abstract_action():
-    # Act and Assert
-    with pytest.raises(TypeError):
-        Action()
+
+# Arrange
+@pytest.fixture(name="mocked_email")
+def fixture_mocked_email():
+    return MagicMock()
+
+
+class TestActionDelete:
+    def test_act(self, mocked_email):
+        # Arrange
+        action_delete = ActionDelete()
+        email_adapter = EmailImapAdapter.instance()
+        email_adapter.delete_msg = MagicMock()
+
+        # Act
+        action_delete.act(mocked_email)
+
+        # Assert
+        email_adapter.delete_msg.assert_called_once_with(mocked_email)
+
+
+class TestActionCopy:
+    def test_act(self, mocked_email):
+        # Arrange
+        action_copy = ActionCopy()
+        email_adapter = EmailImapAdapter.instance()
+        email_adapter.copy_msg = MagicMock()
+
+        # Arrange
+        action_copy.act(mocked_email)
+
+        # Assert
+        email_adapter.copy_msg.assert_called_once_with(mocked_email)
+
+
+class TestActionPrint:
+    def test_act(self, capsys, mocked_email):
+        # Arrange
+        action_print = ActionPrint()
+
+        # Act
+        action_print.act(mocked_email)
+        captured = capsys.readouterr()
+
+        # Assert
+        assert captured.out.strip() == str(mocked_email.sender)
+
+
+class TestActionMove:
+    def test_act(self, mocked_email):
+        # Arrange
+        action_move = ActionMove()
+        email_adapter = EmailImapAdapter.instance()
+        email_adapter.copy_msg = MagicMock()
+        email_adapter.delete_msg = MagicMock()
+
+        # Act
+        action_move.act(mocked_email)
+
+        # Assert
+        email_adapter.copy_msg.assert_called_once_with(mocked_email)
+        email_adapter.delete_msg.assert_called_once_with(mocked_email)
+
+
+class TestAction:
+    def test_act(self, mocked_email):
+        # Act & Assert
+        with pytest.raises(TypeError):
+            Action()
