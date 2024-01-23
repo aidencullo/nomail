280201adc79a6eb3c18374c4ec3934856339c4d0
Aiden Cullo <aidencullo@gmail.com>
2024-01-23 08:36:54 -0500

    mock imaplib instead of Imap constructor

--- tests/imap_test.py
+++ tests/imap_test.py
@@ -6,59 +6,57 @@ from src.imap import Imap
 
 
 MOCK_ARRAY_SIZE = 10
-MOCK_UID = 10
-
-# @pytest.fixture(name="byte_str_mock")
-# def fixture_byte_str():
-#     return b'1 2 3 4 5'
+MOCK_ARRAY = 10 * [None]
+MOCK_MSG = ""
 
 class TestImap:
 
 
-    @patch("src.imap.Imap.__init__", return_value=None, autospec=True)
-    @patch("src.imap.Imap.get_uids", return_value=([None] *
-                                                   MOCK_ARRAY_SIZE), autospec=True)
+    @patch("src.imap.imaplib", autospec=True)
+    @patch("src.imap.Imap.get_uids", return_value=(MOCK_ARRAY), autospec=True)
     @patch("src.imap.Imap.get_msg_data", return_value=None, autospec=True)
-    def test_get_msgs(self, mock_get_msg_data, mock_get_uids, imap_constructor_mock):
+    def test_get_msgs(self, _1, _2, _3):
 
         # Arrange
         imap_mock = Imap()
 
         # Act
-        result = imap_mock.get_msgs()
+        RESULT = imap_mock.get_msgs()
 
         # Assert
-        assert result == [None] * MOCK_ARRAY_SIZE
+        assert RESULT == [None] * MOCK_ARRAY_SIZE
 
 
-    @patch("src.imap.Imap.__init__", return_value=None, autospec=True)
-    @patch("src.imap.email.message_from_bytes", return_value="", autospec=True)
+    @patch("src.imap.imaplib", autospec=True)
+    @patch("src.imap.email.message_from_bytes", return_value=MOCK_MSG, autospec=True)
     @patch("src.imap.Imap.fetch_msg_from_server", autospec=True)
-    def test_get_msg_data(self, mock_get_msg_data, mock_get_uids, imap_constructor_mock):
+    def test_get_msg_data(self, _1, _2, _3):
 
         # Arrange
         imap_mock = Imap()
+        MOCK_UID = 10
 
         # Act
-        result = imap_mock.get_msg_data(MOCK_UID)
+        RESULT = imap_mock.get_msg_data(MOCK_UID)
 
         # Assert
-        assert result == ""
+        assert RESULT == MOCK_MSG
 
 
-    @patch("src.imap.Imap.__init__", return_value=None, autospec=True)
-    def test_fetch_msg_from_server(self, imap_constructor_mock):
-        pass
+    @patch("src.imap.imaplib", autospec=True)
+    def test_fetch_msg_from_server(self, _):
+
         # Arrange
-        imap_mock = Imap()
-        mock_server_msg = 100
-        mock_uid = 100
+        MOCK_SERVER_MSG = 100
+        MOCK_UID = 100
         mock_server_response = np.zeros((2, 2, 2))
-        mock_server_response.fill(mock_server_msg)
+        mock_server_response.fill(MOCK_SERVER_MSG)
+        
+        imap_mock = Imap()
         imap_mock._imap = Mock(fetch=Mock(return_value=mock_server_response))
 
         # Act
-        result = imap_mock.fetch_msg_from_server(mock_uid)
+        RESULT = imap_mock.fetch_msg_from_server(MOCK_UID)
 
         # # Assert
-        assert result == mock_server_msg
+        assert RESULT == MOCK_SERVER_MSG
