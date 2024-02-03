3e41df0a74536d93a8a2183b2c31aab6fe5855f7
Aiden Cullo <aidencullo@gmail.com>
2024-02-03 14:00:49 -0500

    reworking output function
    remove old tests
    
    output is not currently working -- WIP

--- src/output.py
+++ src/output.py
@@ -8,17 +8,17 @@ def create_file_name():
     return re.sub('[-, ]','/', str(datetime.today())) + '.html'
 
 
-def format_data(raw_data):
-    raw_data = list(raw_data)
-    data = {}
-    for key in ['subject', 'from', 'date']:
-        data[key] = [raw_unit[key] for raw_unit in raw_data]
-    return data
+# def format_data(raw_data):
+#     raw_data = list(raw_data)
+#     data = {}
+#     for key in ['subject', 'from', 'date']:
+#         data[key] = [raw_unit[key] for raw_unit in raw_data]
+#     return data
 
 
-def print_file(raw_data, file_name=None):
-    data = format_data(raw_data)
-    if file_name is None:
-        file_name = create_file_name()
-    data_frame = pd.DataFrame(data)
-    data_frame.to_html(file_name, index=False)
+def print_file(raw_data):
+    print(f'{raw_data}')
+    # data = format_data(raw_data)
+    # file_name = create_file_name()
+    # data_frame = pd.DataFrame(data)
+    # data_frame.to_html(file_name, index=False)
--- tests/output_test.py
+++ tests/output_test.py
@@ -3,76 +3,28 @@ from unittest.mock import Mock, patch
 
 import pytest
 
-from src.output import create_file_name, format_data, print_file
-
-
-@pytest.fixture(name='raw_unit_mock')
-def fixture_raw_unit():
-    return {
-        'subject': None,
-        'from': None,
-        'date': None
-    }
-
-
-@pytest.fixture(name='raw_data_mock')
-def fixture_raw_data(raw_unit_mock):
-    return [raw_unit_mock]
+from src import output
 
 class TestOutput:
 
     @pytest.mark.parametrize('datetime_mock, expected', (
         (datetime(2015, 1, 1, 12, 30, 59, 0), '2015/01/01/12:30:59.html'),
-    ),
-                             )
+        (datetime(2015, 2, 1, 12, 30, 59, 0), '2015/02/01/12:30:59.html'),
+        (datetime(2015, 1, 2, 12, 30, 30, 0), '2015/01/02/12:30:30.html'),
+        (datetime(2019, 1, 2, 12, 30, 30, 0), '2019/01/02/12:30:30.html'),
+    ),)
     @patch('src.output.datetime')
-    def test_create_file_name(self, datetime_type_mock, datetime_mock, expected):
+    def test_create_file_name(self, datetime_type_mock, datetime_mock,
+                              expected):
 
         # Arrange
         datetime_type_mock.today = Mock(return_value=datetime_mock)
 
         # Act
-        file_name = create_file_name()
+        file_name = output.create_file_name()
 
         # Assert
         assert file_name == expected
 
-    def test_format_data(self, raw_data_mock):
-
-        # Act
-        result = format_data(raw_data_mock)
-
-        # Assert
-        assert len(result) == len(raw_data_mock[0])
-        assert len(result['subject']) == len(raw_data_mock)
-
-    @patch('src.output.pd.DataFrame.to_html')
-    def test_print_file(self, to_html_mock, raw_data_mock):
-
-        # Act
-        print_file(raw_data_mock)
-
-        # Assert
-        assert to_html_mock.called
-
-    @patch('src.output.create_file_name')
-    @patch('src.output.pd.DataFrame.to_html')
-    def test_print_file_null_file_name(self, to_html_mock,
-                                       create_file_name_mock, raw_data_mock):
-
-        # Act
-        print_file(raw_data_mock)
-
-        # Assert
-        assert create_file_name_mock.called
-
-    @patch('src.output.create_file_name')
-    @patch('src.output.pd.DataFrame.to_html')
-    def test_print_file_present_file_name(self, to_html_mock,
-                                          create_file_name_mock, raw_data_mock):
-
-        # Act
-        print_file(raw_data_mock, "")
-
-        # Assert
-        assert not create_file_name_mock.called
+    def test_print_file(self):
+        pass
