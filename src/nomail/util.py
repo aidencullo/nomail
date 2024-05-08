8cbd199511395a51c657fb8d06f7bd7f6bae799d
Aiden Cullo <aidencullo@gmail.com>
2024-05-07 21:29:05 -0400

    refactor: remove dead to_int function

--- src/nomail/util.py
+++ src/nomail/util.py
@@ -1,9 +1,2 @@
-from typing import Sequence
-
-
 def split_bytes(byte_str: bytes) -> list[bytes]:
     return byte_str.split(b' ') if byte_str else []
-
-
-def to_int(collection: Sequence) -> list[int]:
-    return [int(item) for item in collection]
--- tests/unit/util_test.py
+++ tests/unit/util_test.py
@@ -1,4 +1,4 @@
-from nomail.util import split_bytes, to_int
+from nomail.util import split_bytes
 
 import pytest
 
@@ -12,14 +12,6 @@ class TestUtil:
     def byte_arr(self):
         return [b'1', b'2', b'3', b'4', b'5']
 
-    @pytest.fixture
-    def int_arr(self):
-        return [1, 2, 3, 4, 5]
-
     def test_split_bytes(self, byte_str, byte_arr):
         result = split_bytes(byte_str)
         assert result == byte_arr
-
-    def test_to_int(self, byte_arr, int_arr):
-        result = to_int(byte_arr)
-        assert result == int_arr
