b977f38eecc30815546439585795aa91fcaa0a1c
Aiden Cullo <aidencullo@gmail.com>
2024-05-09 23:37:09 -0400

    refactor: create imap method mocks

--- tests/unit/action_test.py
+++ tests/unit/action_test.py
@@ -22,18 +22,29 @@ class TestAction:
         patcher = patch('nomail.action.Imap')
         return patcher.start().return_value
 
-    def test_delete(self, imap_mock, email_dummy):
+    @pytest.fixture
+    def delete_msg_mock(self, imap_mock):
+        return imap_mock.delete_msg
+
+    @pytest.fixture
+    def copy_msg_mock(self, imap_mock):
+        return imap_mock.copy_msg
+
+    @pytest.fixture
+    def move_msg_mock(self, imap_mock):
+        return imap_mock.move_msg
+
+    def test_delete(self, email_dummy, delete_msg_mock):
         delete_dummy = ActionDelete()
         delete_dummy.act(email_dummy)
-        imap_mock.delete_msg.assert_called_with(email_dummy)
+        delete_msg_mock.assert_called
 
-    def test_copy(self, imap_mock, email_dummy):
+    def test_copy(self, email_dummy, copy_msg_mock):
         copy_dummy = ActionCopy()
         copy_dummy.act(email_dummy)
-        imap_mock.copy_msg.assert_called_with(email_dummy)
+        copy_msg_mock.assert_called
 
-    def test_move(self, imap_mock, email_dummy):
+    def test_move(self, email_dummy, move_msg_mock):
         move_dummy = ActionMove()
         move_dummy.act(email_dummy)
-        imap_mock.copy_msg.assert_called_with(email_dummy)
-        imap_mock.delete_msg.assert_called_with(email_dummy)
+        move_msg_mock.assert_called
